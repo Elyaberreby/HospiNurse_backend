66272fc287fe93ed75e3fdd66fa5c020fd3377a7
Elyaberreby <100205904+Elyaberreby@users.noreply.github.com>
2022-12-22 11:34:13 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const path = require("path");
+const http = require("http");
+const cors = require('cors')
+const { routesInit, corsAccessControl } = require("./routes/config_routes");
+require("./db/mongoConnect");
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.use(express.static(path.join(__dirname, "public")));
+
+routesInit(app);
+
+
+const server = http.createServer(app);
+let port = process.env.PORT || "3004";
+server.listen(port);
